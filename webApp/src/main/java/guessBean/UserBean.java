16c0441a8bf202ca4d2c72f33f710143c9dc4a3e
Nathalia Sanchez perez <nathis@MacBook-Air-de-Nathalia.local>
2023-04-16 18:25:19 -0500

    Pagina de bienvenida

--- webApp/src/main/java/guessBean/UserBean.java
+++ webApp/src/main/java/guessBean/UserBean.java
@@ -10,5 +10,20 @@ import org.springframework.stereotype.Component;
 @ManagedBean
 @ApplicationScoped
 public class UserBean {
+	private String userName;
+	
+	public UserBean() {
+	}
+	
+	/**
+	 * Sets the user name according to the data registered by the user
+	 * @param userName
+	 */
+	public void setUserName(String userName ) {
+		this.userName = userName;
+	}
 
+	public String getUserName() {
+		return userName;
+	}
 }
