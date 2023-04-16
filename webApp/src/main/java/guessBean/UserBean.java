18c7520fc54508884c273f3c0df7502238b9a764
Nathalia Sanchez perez <nathis@MacBook-Air-de-Nathalia.local>
2023-04-16 17:38:17 -0500

    Despliegue de aplicación mediante Spring Boot

---
+++ webApp/src/main/java/guessBean/UserBean.java
@@ -0,0 +1,14 @@
+package guessBean;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@ManagedBean
+@ApplicationScoped
+public class UserBean {
+
+}
